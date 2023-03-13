83b9a0a0c06783aeb8cb5ce65ff9fda3636995a6
Matias Gomez Gamero <matias.gomez.gamero@gmail.com>
2023-03-13 06:57:34 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var n;
+n = 1;
+n = n + 1;
+console.log(n)
+n = 3;
+n = n + 1
+console.log(n)
+n = 5;
+n = n + 1
+console.log(n)
+n = 7;
+n = n + 1;
+console.log(n)
+n = 9;
+n = n + 1
+console.log(n)
