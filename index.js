db07083c4fb9293c199f1566c31d690197ebc6a4
Matias Gomez Gamero <matias.gomez.gamero@gmail.com>
2023-03-14 06:55:11 -0500

    aprendiendo bucles... mientras n sea menor que 10 repita

--- index.js
+++ index.js
@@ -1,17 +1,52 @@
-//contar hasta 20 de 2 en 2 en la mente y hablar de 4 en 4
-var n;
-n = 2;
-n = n + 2;
-console.log(n);
-n = n + 2;
-n = n + 2;
-console.log(n);
-n = n + 2;
-n = n + 2;
-console.log(n);
-n = n + 2;
-n = n + 2;
-console.log(n);
-n = n + 2;
-n = n + 2;
-console.log(n);
+var n = 0;
+
+while (n < 10) {
+    n = n + 2;
+    console.log(n);
+}
+
+while ( n > 5){
+
+}
+
+while (nombre != "matias"){
+
+}
+
+while (eli != "crying"){
+
+}
+
+while (n < 10 ){
+
+}
+
+while (n > 0 ){
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ var n = 0;
+ 
