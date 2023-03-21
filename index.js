2754ea6520e7beac87888d09dab77d1350669d22
Matias Gomez Gamero <matias.gomez.gamero@gmail.com>
2023-03-21 15:05:47 -0500

    tarea

--- index.js
+++ index.js
@@ -2,5 +2,14 @@ import ask from "./utils.js";
 
 (async () => {
   var n = await ask("what number?");
-  console.log(n);
+
+  console.log("hola", "julio");
+
+  //pedir un numero y mostrarlo en pantalla
+
+  //preguntar como se llama y decir "hola " y el nombre
+
+  //preguntar qué edad tiene y decir cuántos años tienes tu menos que la otra persona
+
+  //contar de 1 hasta el numero que pongan
 })();
